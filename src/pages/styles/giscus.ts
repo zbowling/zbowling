840ffafccaadf962a743fad0692b49811d557711
Zac <zac@zacbowling.com>
2026-02-08 16:00:12 -0800

    Switch from SSR to static site generation
    Remove Cloudflare Workers SSR adapter and switch to fully static output.
    This eliminates dev server issues with picomatch/require and Vite SSR
    dependency optimization in the workerd runtime.

--- src/pages/styles/giscus.ts
+++ src/pages/styles/giscus.ts
@@ -1,13 +1,9 @@
-export const prerender = false;
-
 import css from '../../styles/giscus.css?raw';
 
-export async function ALL() {
-  const res = new Response(css);
-
-  res.headers.set('Content-Type', 'text/css');
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-
-  return res;
+export async function GET() {
+  return new Response(css, {
+    headers: {
+      'Content-Type': 'text/css',
+    },
+  });
 }
